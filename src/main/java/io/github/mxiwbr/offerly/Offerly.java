38cb3bb3993508f2037c8a916d3623119f2c5502
mxiwbr <maximilian.web27@gmail.com>
2026-08-08 21:26:21 +0200

    Intial commit

---
+++ src/main/java/io/github/mxiwbr/offerly/Offerly.java
@@ -0,0 +1,16 @@
+package io.github.mxiwbr.offerly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Offerly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
