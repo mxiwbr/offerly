59441d55430e920da8ab25ba08bcb8ff199547a5
mxiwbr <maximilian.web27@gmail.com>
2026-08-13 14:58:33 +0200

    Optimize build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "9.3.1"
+    id("com.gradleup.shadow") version "9.6.1"
 }
 
 group = "io.github.mxiwbr"
@@ -12,24 +12,34 @@ repositories {
 }
 
 dependencies {
+
     // bStats
     implementation("org.bstats:bstats-bukkit:3.2.1")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     // compile only if all features are available in 1.21.11 (plugin works 1.21.11 and newer)
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    implementation("org.apache.commons:commons-text:1.10.0")
+
+    // string utilities
+    implementation("org.apache.commons:commons-text:1.13.1")
+
     // Lombok
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.projectlombok:lombok:1.18.46")
+    annotationProcessor("org.projectlombok:lombok:1.18.46")
+
     // Gson implementation for plugin update service
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.14.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
 
 java {
@@ -38,9 +48,18 @@ java {
 
 tasks.shadowJar {
 
+    // Removes the "-all" from the file name
+    archiveClassifier.set("")
+
     configurations = project.configurations.runtimeClasspath.map { setOf(it) }
 
-    // Relocate bStats into the plugin's package to avoid conflicts with other
-    // plugins using bStats
-    relocate("org.bstats", project.group.toString())
+    // Relocate libraries
+    relocate("org.bstats", "${project.group}.libs.bstats")
+    relocate("com.google.gson", "${project.group}.libs.gson")
+    relocate("org.apache.commons.text", "${project.group}.libs.commonstext")
+
+    // Minimize to reduce file size except GSON implementation
+    minimize {
+        exclude(dependency("com.google.code.gson:gson:.*"))
+    }
 }
